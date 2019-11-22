72bda15f7fbc0affdd0b9f6158df653f49072cc0
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2019-11-22 15:45:31 -0400

    agregado de componentes personas y persona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonasComponent } from './personas/personas.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PersonasComponent, PersonaComponent
   ],
   imports: [
     BrowserModule
