66a4fbb5878be8846e69b652721955efe6850c2e
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2019-11-23 18:29:49 -0400

    aplicacion de property binding y event binding

--- src/app/persona/persona.component.ts
+++ src/app/persona/persona.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonaComponent {
 
-  constructor() { }
+  nombrePersona:string = "Juan";
+  apellidoPersona:string = "Perez";
+  edad:number = 28;
 
 }
--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -2,8 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-personas',
-  templateUrl: './personas.component.html'
+  templateUrl: './personas.component.html',
+  //styleUrls: ['./personas.component.css']
+  styles:[`
+  h1{
+    color:blue;
+  }
+  `]
 })
 export class PersonasComponent{
+  agregarPersona = false;
+  agregarPersonaStatus = "No se ha agregado ninguna persona";
 
+  constructor(){
+    setTimeout(
+      () => {
+          this.agregarPersona = true;
+      }
+      ,3000);
+  }
+      onCrearPersona(){
+        this.agregarPersonaStatus = "persona agregada";
+      }
 }
